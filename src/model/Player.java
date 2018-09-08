9aa0266d01808972d8e2055bc2136046667eaaab
CarlosEduardoL <carlos.lizalda1@correo.icesi.edu.co>
2018-09-08 09:45:50 -0500

    init player class

---
+++ src/model/Player.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Carlos Eduardo Lizalda Valencia
+ * @author Santiago Chasqui Cordoba
+ *
+ */
+public class Player {
+	
+	private String nickName;
+	private int kills;
+
+}
